7d27ba0a78bbb26280de20b0c974e255def6ab09
53845714nF <seba98feustel@gmail.com>
2024-02-17 14:52:45 +0100

    Add Chacking for time via serial console

--- time2boot.ino
+++ time2boot.ino
@@ -4,39 +4,60 @@
 RTC_DS3231 rtc;
 int optocoupler = 2;
 
-void setup () {
-  pinMode(optocoupler, OUTPUT);
-  digitalWrite(optocoupler, LOW);
-  
-  Serial.begin(9600);
+void setup()
+{
+    pinMode(optocoupler, OUTPUT);
+    digitalWrite(optocoupler, LOW);
 
-  if (! rtc.begin()) {
-    Serial.println("Couldn't find RTC");
-    Serial.flush();
-    abort();
-  }
+    Serial.begin(9600);
 
-  if (rtc.lostPower()) {
-    Serial.println("RTC lost power, let's set the time!");
-    // When time needs to be set on a new device, or after a power loss, the
-    // following line sets the RTC to the date & time this sketch was compiled
-    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
-  }
+    if (!rtc.begin())
+    {
+        Serial.println("Couldn't find RTC");
+        Serial.flush();
+        abort();
+    }
 
+    if (rtc.lostPower())
+    {
+        Serial.println("RTC lost power, let's set the time!");
+        // When time needs to be set on a new device, or after a power loss, the
+        // following line sets the RTC to the date & time this sketch was
+        // compiled
+        rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+    }
 }
 
-void loop () {
-  DateTime now = rtc.now();
-  
-  if (now.timestamp(DateTime::TIMESTAMP_TIME) == "00:00:00"){
-    computerOn();
-  }
+void loop()
+{
+    DateTime now = rtc.now();
 
-  delay(1000);
+    if (Serial.available() > 0)
+    {
+        int input = Serial.read();
+
+        switch (input)
+        {
+        case 't':
+            Serial.println(now.timestamp(DateTime::TIMESTAMP_TIME));
+            break;
+        case 's':
+            computerOn();
+            break;
+        default:
+            break;
+        }
+    }
+
+    if (now.timestamp(DateTime::TIMESTAMP_TIME) == "00:00:00")
+    {
+        computerOn();
+    }
 }
 
-void computerOn(){
-  digitalWrite(optocoupler, HIGH);
-  delay(2000);
-  digitalWrite(optocoupler, LOW);
+void computerOn()
+{
+    digitalWrite(optocoupler, HIGH);
+    delay(2000);
+    digitalWrite(optocoupler, LOW);
 }
